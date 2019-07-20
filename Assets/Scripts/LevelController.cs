dc54409f0748a6c6927fc424303437e8d468c265
Richard Estes <richard.estes48@gmail.com>
2019-07-20 14:09:01 -0700

    Initial Development

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject platform;
+    public PointEffector2D pointEffector;
+
+    private void Start()
+    {
+        platform.GetComponent<PointEffector2D>().enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            platform.GetComponent<PointEffector2D>().enabled = true;
+        if (Input.GetKeyUp(KeyCode.Space))
+            platform.GetComponent<PointEffector2D>().enabled = false;
+    }
+}
